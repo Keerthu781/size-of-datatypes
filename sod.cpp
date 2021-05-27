b94fdf8d491c619a72aec40376d9ea2b36a7b20a
Keerthu781 <80473077+Keerthu781@users.noreply.github.com>
2021-05-27 19:00:25 +0530

    create sod.cpp
    It helps to find the size of int ,float ,char, double.

---
+++ sod.cpp
@@ -0,0 +1,9 @@
+include <iostream>
+using namespace std;
+int main() {
+   cout<<"\n\tSize of int is    ::"<<sizeof(int)<<" bytes";
+   cout<<"\n\tSize of float is  ::"<<sizeof(float)<<" bytes";
+   cout<<"\n\tSize of double is ::"<<sizeof(double)<<" bytes";
+   cout<<"\n\tSize of char is   ::"<<sizeof(char)<<" byte";
+   return 0;
+}
